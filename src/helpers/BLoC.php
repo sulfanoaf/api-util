579e20398e1db97e352cf1984ac602ce55be24a9
Sulfano Agus Fikri <sulfano.fikri@flash-coffee.com>
2021-07-22 22:37:45 +0700

    add default value BLoC

--- src/helpers/BLoC.php
+++ src/helpers/BLoC.php
@@ -5,7 +5,7 @@ use App;
 
 class BLoC
 {
-    public static function call($blocClass, $params) {
+    public static function call($blocClass, $params = []) {
         $bloc = App::make($blocClass);
         $blocParams = new BLoCParams($params);
         return $bloc->execute($blocParams);
